601790d306e6a2eec77ae838cb030894b6ee55aa
jiyinyiyong <jiyinyiyong@gmail.com>
2018-04-14 18:44:11 +0800

    switching to single project workflow

---
+++ src/app/twig/container.cljs
@@ -0,0 +1,20 @@
+
+(ns app.twig.container
+  (:require [recollect.macros :refer [deftwig]] [app.twig.user :refer [twig-user]]))
+
+(deftwig
+ twig-container
+ (db session records)
+ (let [logged-in? (some? (:user-id session))
+       router (:router session)
+       base-data {:logged-in? logged-in?,
+                  :session session,
+                  :count (:count db),
+                  :reel-length (count records)}]
+   (merge
+    base-data
+    (if logged-in?
+      {:user (twig-user (get-in db [:users (:user-id session)])),
+       :router router,
+       :count (count (:sessions db))}
+      nil))))
