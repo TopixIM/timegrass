ba176497d33d0cd9488fcc0fcb58557ae58cbaec
jiyinyiyong <jiyinyiyong@gmail.com>
2018-09-30 00:57:20 +0800

    some style changes in history page

--- src/app/comp/history.cljs
+++ src/app/comp/history.cljs
@@ -15,7 +15,10 @@
  (states task)
  (let [state (or (:data states) {:show-menu? false})]
    (div
-    {:style (merge ui/row {:padding "4px 0"}),
+    {:style (merge
+             ui/row
+             {:padding "4px 16px"}
+             (when (:show-menu? state) {:background-color (hsl 0 0 94)})),
      :on-click (fn [e d! m!] (m! (assoc state :show-menu? true)))}
     (div
      {:style {:min-width 40, :color (hsl 0 0 80), :font-size 12}}
@@ -32,32 +35,36 @@
  comp-history
  (states finished-tasks)
  (div
-  {:style (merge ui/flex {:padding "8px 16px", :overflow :auto})}
+  {:style (merge ui/flex {:padding "8px 8px", :overflow :auto})}
+  (div
+   {:style {:padding "0 8px",
+            :font-family ui/font-fancy,
+            :font-size 24,
+            :color (hsl 0 0 80)}}
+   (<> "Histories"))
   (let [grouped-tasks (->> (vals finished-tasks)
                            (group-by
                             (fn [task] (.format (dayjs (:finished-time task)) "YYYY-MM-DD"))))]
-    (div
+    (list->
      {}
-     (list->
-      {}
-      (->> grouped-tasks
-           (sort (fn [x y] (compare (first y) (first x))))
-           (map
-            (fn [[date-string task-list]]
-              [date-string
-               (div
-                {:style (merge ui/column {:margin-top 24})}
-                (let [the-day (dayjs date-string)]
-                  (span
-                   {:style {:font-family ui/font-fancy}}
-                   (<> (.format the-day "ddd"))
-                   (=< 12 nil)
-                   (<> (.format the-day "MM-DD"))))
-                (=< nil 4)
-                (list->
-                 {}
-                 (->> task-list
-                      (sort-by (fn [task] (unchecked-negate (:finished-time task))))
-                      (map
-                       (fn [task]
-                         [(:id task) (cursor-> (:id task) comp-done-task states task)])))))]))))))))
+     (->> grouped-tasks
+          (sort (fn [x y] (compare (first y) (first x))))
+          (map
+           (fn [[date-string task-list]]
+             [date-string
+              (div
+               {:style (merge ui/column {:margin-top 24})}
+               (let [the-day (dayjs date-string)]
+                 (span
+                  {:style {:font-family ui/font-fancy, :padding "0 8px"}}
+                  (<> (.format the-day "ddd"))
+                  (=< 12 nil)
+                  (<> (.format the-day "MM-DD"))))
+               (=< nil 4)
+               (list->
+                {}
+                (->> task-list
+                     (sort-by (fn [task] (unchecked-negate (:finished-time task))))
+                     (map
+                      (fn [task]
+                        [(:id task) (cursor-> (:id task) comp-done-task states task)])))))])))))))
