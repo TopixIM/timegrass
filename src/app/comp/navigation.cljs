442b67a6c166e74883d15214982e5e8b72efd9b7
jiyinyiyong <jiyinyiyong@gmail.com>
2021-03-25 00:37:26 +0800

    fix dynamic date switching again

--- src/app/comp/navigation.cljs
+++ src/app/comp/navigation.cljs
@@ -27,33 +27,33 @@
 (defcomp
  comp-navigation
  (logged-in? count-members page)
- (let [today (dayjs)
-       year (.year today)
-       month (.month today)
-       week (let [w (.week today)]
-              (if (and (== month 11) (> (.date today) 25))
-                (inc (.week (.subtract today 7 "day")))
-                w))]
+ (div
+  {:style style-navbar}
+  (div
+   {:style (merge ui/row-parted {:max-width 840, :width "100%", :margin :auto})}
    (div
-    {:style style-navbar}
-    (div
-     {:style (merge ui/row-parted {:max-width 840, :width "100%", :margin :auto})}
-     (div
-      {:style (merge ui/row-center)}
-      (render-entry "Timegrass" (fn [] {:name :home}) (= page :home))
-      (=< 16 nil)
-      (render-entry
-       "Finished"
-       (fn [] {:name :history, :data {:year year, :week week}})
-       (= page :history))
-      (=< 16 nil)
-      (render-entry
-       "Notes"
-       (fn [] {:name :notes, :data (let [now (dayjs)] {:year year, :month month})})
-       (= page :notes)))
-     (div
-      {:style {:cursor "pointer"},
-       :on-click (fn [e d!] (d! :router/change {:name :profile}))}
-      (<> (if logged-in? "Me" "Guest"))
-      (=< 8 nil)
-      (<> count-members))))))
+    {:style (merge ui/row-center)}
+    (render-entry "Timegrass" (fn [] {:name :home}) (= page :home))
+    (=< 16 nil)
+    (render-entry
+     "Finished"
+     (fn []
+       {:name :history,
+        :data (let [now (dayjs), month (.month now)]
+          {:year (.year now),
+           :week (let [w (.week now)]
+             (if (and (== month 11) (> (.date now) 25))
+               (inc (.week (.subtract now 7 "day")))
+               w))})})
+     (= page :history))
+    (=< 16 nil)
+    (render-entry
+     "Notes"
+     (fn []
+       {:name :notes, :data (let [now (dayjs)] {:year (.year now), :month (.month now)})})
+     (= page :notes)))
+   (div
+    {:style {:cursor "pointer"}, :on-click (fn [e d!] (d! :router/change {:name :profile}))}
+    (<> (if logged-in? "Me" "Guest"))
+    (=< 8 nil)
+    (<> count-members)))))
